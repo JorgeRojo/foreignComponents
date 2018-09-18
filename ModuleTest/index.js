3d75e780c21f77311cb8ae8a34440a456ad971ab
Jorge Rojo <jorge.rojodiseno@gmail.com>
2018-09-19 00:18:11 +0200

    ModuleTest created

---
+++ ModuleTest/index.js
@@ -0,0 +1,13 @@
+import React from 'react';  
+import CSSModules from 'react-css-modules';
+import styles from './styles.scss';     
+
+function ModuleTest(props) {    
+    return (
+        <div styleName="wrapper">
+            <p>This is my foreign module</p>
+        </div>
+    );
+}
+ 
+export default CSSModules(ModuleTest, styles);
